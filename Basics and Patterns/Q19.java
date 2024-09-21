38e754d0cc451e7c8707fc2f52830494e11aac92
Anirban Chatterjee <ani.kol2099@gmail.com>
2024-09-22 00:23:34 +0530

    trial code

---
+++ Basics and Patterns/Q19.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public  class Q19 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if (age  >= 18){
+            System.out.println("You are eligible to vote");
+            }
+            
+        else{
+            System.out.println("Not eligiblento vote");
+        }
+
+        }
+    }
+        
+   
